6865dd84aa15a4297dec37211c07b146c1b0c4e1
Dashgin <dsdsds2103@gmail.com>
2024-08-17 17:07:15 +0400

    feat(service): Add logging context manager function and related unit test

--- tests/log/test_log.py
+++ tests/log/test_log.py
@@ -4,13 +4,36 @@ from io import StringIO
 from src.pytemplate.service.logs import log
 
 
-def test_log_context_manager():
+def test_log_context_manager_for_info():
     log_stream = StringIO()
     handler = logging.StreamHandler(log_stream)
     logger = logging.getLogger()
     logger.addHandler(handler)
 
-    original_level = logging.INFO
+    original_level = logging.DEBUG
+    logger.setLevel(original_level)
+
+    with log(logging.INFO) as logger:
+        assert logger.getEffectiveLevel() == logging.INFO
+        logger.info("This is an info message")
+
+    # Ensure the logging level is restored after context manager exits
+    assert logger.getEffectiveLevel() == original_level
+
+    # Check that the info message was logged
+    log_contents = log_stream.getvalue()
+    assert "This is an info message" in log_contents
+
+    log_stream.close()
+
+
+def test_log_context_manager_for_warning():
+    log_stream = StringIO()
+    handler = logging.StreamHandler(log_stream)
+    logger = logging.getLogger()
+    logger.addHandler(handler)
+
+    original_level = logging.DEBUG
     logger.setLevel(original_level)
 
     with log(logging.WARNING) as logger:
@@ -23,5 +46,28 @@ def test_log_context_manager():
     # Check that the warning message was logged
     log_contents = log_stream.getvalue()
     assert "This is a warning message" in log_contents
+
+    log_stream.close()
+
+
+def test_log_context_manager_for_error():
+    log_stream = StringIO()
+    handler = logging.StreamHandler(log_stream)
+    logger = logging.getLogger()
+    logger.addHandler(handler)
+
+    original_level = logging.DEBUG
+    logger.setLevel(original_level)
+
+    with log(logging.ERROR) as logger:
+        assert logger.getEffectiveLevel() == logging.ERROR
+        logger.error("This is an error message")
+
+    # Ensure the logging level is restored after context manager exits
+    assert logger.getEffectiveLevel() == original_level
+
+    # Check that the error message was logged
+    log_contents = log_stream.getvalue()
+    assert "This is an error message" in log_contents
 
     log_stream.close()
