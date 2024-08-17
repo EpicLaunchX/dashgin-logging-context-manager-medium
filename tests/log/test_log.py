f563d73cbe1ca16e676e090774fb819790bc49b1
Dashgin <dsdsds2103@gmail.com>
2024-08-17 17:04:45 +0400

    feat(service): Add logging context manager function and related unit test

--- tests/log/test_log.py
+++ tests/log/test_log.py
@@ -1,8 +1,6 @@
 import logging
 from io import StringIO
 
-import pytest
-
 from src.pytemplate.service.logs import log
 
 
@@ -15,15 +13,15 @@ def test_log_context_manager():
     original_level = logging.INFO
     logger.setLevel(original_level)
 
-    with log(logging.DEBUG) as logger:
-        assert logger.getEffectiveLevel() == logging.DEBUG
-        logger.debug("This is a debug message")
+    with log(logging.WARNING) as logger:
+        assert logger.getEffectiveLevel() == logging.WARNING
+        logger.warning("This is a warning message")
 
     # Ensure the logging level is restored after context manager exits
     assert logger.getEffectiveLevel() == original_level
 
-    # Check that the debug message was logged
+    # Check that the warning message was logged
     log_contents = log_stream.getvalue()
-    assert "This is a debug message" in log_contents
+    assert "This is a warning message" in log_contents
 
     log_stream.close()
